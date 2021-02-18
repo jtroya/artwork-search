577c61316a44c4d3fa733a3a5b7f1e07f16c0000
Fernando Ramos <ferramgon1@gmail.com>
2021-02-18 19:07:51 +0100

    fixes file extension and adds missing typings

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render as rtlRender, RenderOptions } from '@testing-library/react';
-import { Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import { store } from './store';
 
 const render = (
   ui: React.ReactElement,
   renderOptions?: Omit<RenderOptions, 'queries'>,
-) => {
+): React.ReactNode => {
   const Wrapper: React.FC = ({ children }) => {
     return (
       <Provider store={store}>
-        <Router>{children}</Router>
+        <Route>{children}</Route>
       </Provider>
     );
   };
