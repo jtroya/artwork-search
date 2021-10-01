c85bf4edbc6068fbd9e7333e957fcc69716a0edc
Jorge Troya <jorgetroya@gmail.com>
2021-10-01 15:23:34 +0200

    feat: install Storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+import '../src/styles.css';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: '#f9f9fb',
+      },
+    ],
+  },
+};
